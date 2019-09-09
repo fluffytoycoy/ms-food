2c3cffd1e7717ac20ec3cd2d0608aaa78686df55
Matt Coy <fluffytoycoy@gmail.com>
2019-09-08 22:38:47 -0700

    added gallery

--- front/src/components/Gallery/ImgGallery.js
+++ front/src/components/Gallery/ImgGallery.js
@@ -1,47 +1,134 @@
 import React from 'react';
 import { render } from 'react-dom';
 import Gallery from 'react-grid-gallery';
-
+import './ImgGallery.scss'
 
 function ImgGallery(){
   const IMAGES =[
-    {
-        src: "/img/Backgrounds/about_us.jpg",
-        thumbnail: "/img/Backgrounds/about_us.jpg",
-        caption: "After Rain (Jeshu John - designerspics.com)"
-      },
+
       {
         src: "/img/Backgrounds/gallery.jpg",
         thumbnail: "/img/Backgrounds/gallery.jpg",
-        caption: "Boats (Jeshu John - designerspics.com)"
-      },
-      {
-        src: "/img/Backgrounds/info_bg.jpg",
-        thumbnail: "/img/Backgrounds/info_bg.jpg",
+        thumbnailHeight: 500,
       },
       {
         src: "/img/Backgrounds/venue.jpg",
         thumbnail: "/img/Backgrounds/venue.jpg",
+        thumbnailHeight: 500,
       },
 
       {
         src: "/img/Gallery/baseballs.jpg",
         thumbnail: "/img/Gallery/baseballs.jpg",
+        thumbnailHeight: 500,
       },
       {
-        src: "/img/Backgrounds/info_bg.jpg",
-        thumbnail: "/img/Backgrounds/info_bg.jpg",
-      }
+        src: "/img/Gallery/bacon_wrap.jpg",
+        thumbnail: "/img/Gallery/bacon_wrap.jpg",
+        thumbnailHeight: 500,
+        caption: "Bacon Wraps",
+      },
+      {
+        src: "/img/Gallery/bar_nuts.jpg",
+        thumbnail: "/img/Gallery/bar_nuts.jpg",
+        thumbnailHeight: 500,
+      },
+      {
+        src: "/img/Gallery/beef_mushrooms.jpg",
+        thumbnail: "/img/Gallery/beef_mushrooms.jpg",
+        thumbnailHeight: 500,
+        caption: "Mushroom Steak",
+      },
+      {
+        src: "/img/Gallery/booths.jpg",
+        thumbnail: "/img/Gallery/booths.jpg",
+        thumbnailHeight: 500,
+      },
+      {
+        src: "/img/Gallery/buffalo_whiskey.jpg",
+        thumbnail: "/img/Gallery/buffalo_whiskey.jpg",
+        thumbnailHeight: 500,
+        caption: "buffalo ol' Fashioned",
+      },
+      {
+        src: "/img/Gallery/chocolate_dessert.jpg",
+        thumbnail: "/img/Gallery/chocolate_dessert.jpg",
+        thumbnailHeight: 500,
+        caption: "crème glacée au chocolat",
+      },
+      {
+        src: "/img/Gallery/eggs.jpg",
+        thumbnail: "/img/Gallery/eggs.jpg",
+        thumbnailHeight: 500,
+        caption: "oeufs farcis",
+      },
+      {
+        src: "/img/Gallery/fruit_drink.jpg",
+        thumbnail: "/img/Gallery/fruit_drink.jpg",
+        thumbnailHeight: 500,
+      },
+      {
+        src: "/img/Gallery/lemon_meringue.jpg",
+        thumbnail: "/img/Gallery/lemon_meringue.jpg",
+        thumbnailHeight: 500,
+      },
+      {
+        src: "/img/Gallery/Mojito.jpg",
+        thumbnail: "/img/Gallery/beef_mushrooms.jpg",
+        thumbnailHeight: 500,
+      },
+      {
+        src: "/img/Gallery/Mojito.jpg",
+        thumbnail: "/img/Gallery/beef_mushrooms.jpg",
+        thumbnailHeight: 500,
+      },
+      {
+        src: "/img/Gallery/pork.jpg",
+        thumbnail: "/img/Gallery/pork.jpg",
+        thumbnailHeight: 500,
+      },
+      {
+        src: "/img/Gallery/ranch_burger.jpg",
+        thumbnail: "/img/Gallery/ranch_burger.jpg",
+        thumbnailHeight: 500,
+      },
+      {
+        src: "/img/Gallery/samon.jpg",
+        thumbnail: "/img/Gallery/samon.jpg",
+        thumbnailHeight: 500,
+      },
+      {
+        src: "/img/Gallery/sports_rack.jpg",
+        thumbnail: "/img/Gallery/sports_rack.jpg",
+        thumbnailHeight: 500,
+      },
+      {
+        src: "/img/Gallery/steak.jpg",
+        thumbnail: "/img/Gallery/steak.jpg",
+        thumbnailHeight: 500,
+      },
+      {
+        src: "/img/Gallery/strawberry.jpg",
+        thumbnail: "/img/Gallery/strawberry.jpg",
+        thumbnailHeight: 500,
+      },
+
     ]
 
   return(
-    <section id="home">
-    <div>
-        <Gallery
-        images={IMAGES}
-        enableLightbox={true}
-        enableImageSelection={false}/>
-    </div>
+    <section id="gallery">
+      <div className="container">
+      <div className= "title-bar">
+        <div>
+          <h3>Gallery</h3>
+        </div>
+      </div>
+          <Gallery
+          images={IMAGES}
+          enableLightbox={true}
+          rowHeight={300}
+          enableImageSelection={false}/>
+          </div>
     </section>
   )
 
