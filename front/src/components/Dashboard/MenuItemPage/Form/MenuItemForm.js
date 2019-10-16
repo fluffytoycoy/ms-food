a929eeda4427864e8ce98ce6effc147c949dc048
Matt Coy <fluffytoycoy@gmail.com>
2019-10-16 10:30:59 -0700

    switch added for changing the menu display

--- front/src/components/Dashboard/MenuItemPage/Form/MenuItemForm.js
+++ front/src/components/Dashboard/MenuItemPage/Form/MenuItemForm.js
@@ -6,7 +6,12 @@ import { connect } from 'react-redux'
 
 
 function MenuItemForm(props){
+  const [name, setName] = useState(props.selectedMenuItem ? props.selectedMenuItem.name : '')
   const [type, setType] = useState(props.selectedMenuItem ? props.selectedMenuItem.type : '')
+  const [price, setPrice] = useState(props.selectedMenuItem ? props.selectedMenuItem.price : '')
+  const [served, setServed] = useState(props.selectedMenuItem ? props.selectedMenuItem.served : '')
+  const [subtype, setSubtype] = useState(props.selectedMenuItem ? props.selectedMenuItem.subtype : '')
+  const [ingredients, setIngredients] = useState(props.selectedMenuItem ? props.selectedMenuItem.ingredients : '')
 
   const getSelectOptions = (property) => {
     const keys = Object.entries(props[property]);
@@ -22,11 +27,33 @@ function MenuItemForm(props){
   }
 
   const formchange =(event)=>{
-    if(event.target.name === 'type'){
-      setType('test')
+    switch(event.target.name){
+      case 'type':
+        const typeOptions = props['types'];
+        setType(getStringifiedKeyFromValue(typeOptions, event.target.value));
+        break;
+      case 'name':
+        setName(event.target.value);
+        break;
+      case 'price':
+        setPrice(event.target.value);
+        break;
+      case 'served':
+        setServed(event.target.value);
+        break;
+      case 'subtype':
+        setSubtype(event.target.value);
+        break;
+      case 'ingredients':
+        setIngredients(event.target.value)
+        break;
+      default:
+        break;
     }
-    console.log(event.target.name)
-    console.log(event.target.options)
+
+    function getStringifiedKeyFromValue(object, value){
+      return Object.keys(object).filter(key => object[key] == value)[0];
+    }
   }
 
   const gameForm = () =>{
@@ -50,9 +77,9 @@ function MenuItemForm(props){
         <div class="menu-cols" data-category="Drinks">
           <h4>{type}</h4>
           <div data-category="Drinks">
-          <h6>Bloody Mary $8 </h6>
-          <p>-Served Tall in Soda Glass-</p>
-          <p>Ménage à Trois Vodka, Tres Agaves Bloody Mary Mix, Dash Mike Shannon's Steak Sauce, Salt</p>
+            <h6>{name} {price}</h6>
+            {served ? <p>{served}</p> : <></>}
+            {ingredients ? <p>{ingredients}</p> : <></>}
           </div>
         </div>
       </div>
