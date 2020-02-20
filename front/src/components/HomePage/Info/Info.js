f8161cf4e2bf38e5534a80bb63faa86c64dae95a
Matt Coy <fluffytoycoy@gmail.com>
2020-02-20 07:31:48 -0600

    style changes to landing page

--- front/src/components/HomePage/Info/Info.js
+++ front/src/components/HomePage/Info/Info.js
@@ -6,36 +6,35 @@ function Info(){
   return(
     <section id="info">
       <div className="info-wrapper">
-          <ScrollAnimation animateOnce animateIn="fadeInLeft">
-          <div>
-            <h2>Happy Hour</h2>
-            <div className="times">
-              <p>3-5:30PM Monday-Thursday</p>
-              <p>9-10PM Friday and Saturday Night</p>
-            </div>
-            <div className="featured-meals">
-              <p>1/2 Price Appetizers<span>* (Excludes Crab Dip, Crab Cakes and Pork Shanks)</span></p>
-              <p>$3 Wells</p>
-              <p>$2 Bud Light, Busch, Budweiser Drafts</p>
-              <p>$5 Select Cabernet and Chardonnay</p>
-              <p>Patio and Bar Only</p>
-            </div>
-          </div>
-          </ScrollAnimation>
-          <ScrollAnimation animateOnce  animateIn="fadeInRight">
-            <div>
-              <h2>Brunch</h2>
+        <h2>Happy Hour</h2>
+          <div className="info-body">
+            <ScrollAnimation className="info-animate" animateOnce animateIn="fadeInLeft">
               <div className="times">
-                <p>Saturdays 11AM-2PM and Sundays 10AM-2PM</p>
+                <p>Monday - Thursday</p>
+                <div className="happy-hour-numbers">
+                  <span>3</span>
+                  <span className="dash">-</span>
+                  <span>6</span>
+                  <span className="pm">PM</span>
+                </div>
+                <p>Friday - Sunday</p>
+                <div className="happy-hour-numbers">
+                  <span>8</span>
+                  <span className="dash">-</span>
+                  <span>10</span>
+                  <span className="pm">PM</span>
+                </div>
               </div>
+            </ScrollAnimation>
+            <ScrollAnimation className="info-animate" animateOnce animateIn="fadeInRight">
               <div className="featured-meals">
-                <p>$4 Biscuits & Gravy</p>
-                <p>$10 Breakfast Bowls</p>
-                <p>$10 Crêpes</p>
-                <p>$9 Pancakes & French Toast Meals</p>
+                <p>1/2 Price Appetizers</p>
+                <p>$3 Wells</p>
+                <p>$2 Bud Light, Busch, Budweiser Drafts</p>
+                <p>$5 Select Cabernet and Chardonnay</p>
               </div>
-            </div>
-          </ScrollAnimation>
+            </ScrollAnimation>
+          </div>
         </div>
     </section>
   )
